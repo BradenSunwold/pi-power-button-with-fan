3ffc98091e0954101ca036ef9de63c7a812bbb1b
Braden Sunwold <sunwoldb@gmail.com>
2022-09-17 17:52:03 -0700

    Added GPIO17 as cooling fan power enable

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -3,10 +3,14 @@
 
 import RPi.GPIO as GPIO
 import subprocess
+import time
 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, True)
+time.sleep(0.01)
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
 subprocess.call(['shutdown', '-h', 'now'], shell=False)
