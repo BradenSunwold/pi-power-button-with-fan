745471f60df7232a83f1593a26f95e57e51248f7
Braden Sunwold <sunwoldb@gmail.com>
2024-06-08 11:57:48 -0700

    Updated listenForShutdown to be compatable with Pi4 and Pi5

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -5,12 +5,18 @@ import RPi.GPIO as GPIO
 import subprocess
 import time
 
+def WaitForShutdownEdge() :
+    currentLevel = GPIO.input(3)
+    while(GPIO.input(3) == currentLevel) :
+        time.sleep(.01)
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN)
 GPIO.setup(17, GPIO.OUT)
 GPIO.output(17, True)
 time.sleep(0.01)
-GPIO.wait_for_edge(3, GPIO.FALLING)
+#GPIO.wait_for_edge(3, GPIO.FALLING)
+WaitForShutdownEdge()
 
 subprocess.call(['shutdown', '-h', 'now'], shell=False)
